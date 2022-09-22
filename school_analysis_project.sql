b75694808102250f0beb79914b47e5acacb4ea0a
JosephWamitiMugo <91910681+JosephWamiti8711@users.noreply.github.com>
2022-09-22 10:40:29 +0300

    Add files via upload

--- school_analysis_project.sql
+++ school_analysis_project.sql
@@ -43,9 +43,31 @@ INSERT INTO Students_Table (student_id,student_fname,student_lname,class,age) VA
 INSERT INTO Students_Table (student_id,student_fname,student_lname,class,age) VALUES(10,'sara',	'rayan',10,16);
 INSERT INTO Students_Table (student_id,student_fname,student_lname,class,age) VALUES(11,'rosy',	'kumar',10,16);
 
-
+INSERT INTO Marksheet(score,years,ranking,class,student_id) VALUES (989,2014,10,1,1);
+INSERT INTO Marksheet(score,years,ranking,class,student_id) VALUES (454,2014,10,10,2);
+INSERT INTO Marksheet(score,years,ranking,class,student_id) VALUES (880,2014,10,4,3);
+INSERT INTO Marksheet(score,years,ranking,class,student_id) VALUES (870,2014,10,5,4);
+INSERT INTO Marksheet(score,years,ranking,class,student_id) VALUES (720,2014,10,7,5);
+INSERT INTO Marksheet(score,years,ranking,class,student_id) VALUES (670,2014,10,8,6);
+INSERT INTO Marksheet(score,years,ranking,class,student_id) VALUES (900,2014,10,3,7);
+INSERT INTO Marksheet(score,years,ranking,class,student_id) VALUES (540,2014,10,9,8);
+INSERT INTO Marksheet(score,years,ranking,class,student_id) VALUES (801,2014,10,6,9);
+INSERT INTO Marksheet(score,years,ranking,class,student_id) VALUES (420,2014,10,11,10);
+INSERT INTO Marksheet(score,years,ranking,class,student_id) VALUES (970,2014,10,2,11);
+INSERT INTO Marksheet(score,years,ranking,class,student_id) VALUES (720,2014,10,12,12);
 -- Write a query to display student id and student first name from the student table if the age is greater than or equal to 16 and the student's last name is Kumar.
+
+SELECT student_id,student_fname FROM Students_Table WHERE age >= 16 AND student_lname = 'kumar'; 
+
 -- Write a query to display all the details from the marksheet table if the score is between 800 and 1000.
+
+SELECT * FROM Marksheet WHERE score BETWEEN 800 AND 1000;
 -- Write a query to display the marksheet details from the marksheet table by adding 5 to the score and by naming the column as new score.
+
+ALTER TABLE marksheet ADD COLUMN new_score int;
+SELECT score + 5 FROM marksheet AS new_score;
+
 -- Write a query to display the marksheet table in descending order of the  score.
+SELECT * FROM Marksheet ORDER BY score desc;
 -- Write a query to display details of the students whose first name starts with a.
+SELECT * FROM Students_Table WHERE student_fname like "%a";
